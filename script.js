a9ab6ced62c9296458e662006cc8ffc6f3c3cb65
Andrei Ciobanita <al.ciobanita@gmail.com>
2024-04-29 15:01:20 +0200

    Endscreen-Update: Anzahl richtige fragen wird angezeigt

--- script.js
+++ script.js
@@ -1,39 +1,40 @@
 let questions = [
-  // {
-  //   "question": "Wer hat HTML erfunden?",
-  //   "answer_1": "Lady Gaga",
-  //   "answer_2": "Richard Stallman",
-  //   "answer_3": "Tim Berners-Lee",
-  //   "answer_4": "Mark Zuckerberg",
-  //   "right_answer": 3
-  // },
-  // {
-  //   "question": "Welches Attribut kann man NICHT für Textarea verwenden?",
-  //   "answer_1": "readonly",
-  //   "answer_2": "max",
-  //   "answer_3": "from",
-  //   "answer_4": "spellcheck",
-  //   "right_answer": 1
-  // },
-  // {
-  //   "question": "Wie wählst du alle Elemente vom Typ &lt;a&gt; mit dem attribut title aus?",
-  //   "answer_1": "a[title]{...}",
-  //   "answer_2": "a>title{...}",
-  //   "answer_3": "a.title{...}",
-  //   "answer_4": "a=title{...}",
-  //   "right_answer": 1
-  // },
-  // {
-  //   "question": "Wie definiert man in JavaScript eine Variable?",
-  //   "answer_1": "let 100 = rate;",
-  //   "answer_2": "100= let rate",
-  //   "answer_3": "rate = 100",
-  //   "answer_4": "let rate = 100;",
-  //   "right_answer": 4
-  // }
+  {
+    "question": "Wer hat HTML erfunden?",
+    "answer_1": "Lady Gaga",
+    "answer_2": "Richard Stallman",
+    "answer_3": "Tim Berners-Lee",
+    "answer_4": "Mark Zuckerberg",
+    "right_answer": 3
+  },
+  {
+    "question": "Welches Attribut kann man NICHT für Textarea verwenden?",
+    "answer_1": "readonly",
+    "answer_2": "max",
+    "answer_3": "from",
+    "answer_4": "spellcheck",
+    "right_answer": 1
+  },
+  {
+    "question": "Wie wählst du alle Elemente vom Typ &lt;a&gt; mit dem attribut title aus?",
+    "answer_1": "a[title]{...}",
+    "answer_2": "a>title{...}",
+    "answer_3": "a.title{...}",
+    "answer_4": "a=title{...}",
+    "right_answer": 1
+  },
+  {
+    "question": "Wie definiert man in JavaScript eine Variable?",
+    "answer_1": "let 100 = rate;",
+    "answer_2": "100= let rate",
+    "answer_3": "rate = 100",
+    "answer_4": "let rate = 100;",
+    "right_answer": 4
+  }
 ];
 
 let currentQuestion = 0;
+let rightQuestions = 0;
 
 function init() {
   document.getElementById('all-questions').innerHTML = questions.length;
@@ -45,6 +46,9 @@ function showQuestion() {
   if (currentQuestion >= questions.length) {
     document.getElementById('endScreen').style = "";
     document.getElementById('questionBody').style= "display: none";
+  
+    document.getElementById('amountOfQuestions').innerHTML = questions.length;
+    document.getElementById('amount-of-right-questions').innerHTML = rightQuestions;
   } else {
     let question = questions[currentQuestion];
 
@@ -64,8 +68,8 @@ function answer(selection) {
   let idOfRightAnswer = `answer_${question['right_answer']}`;
 
   if (selectedQuestionNumber == question[`right_answer`]) {
-    console.log('Richtige Antwort!');
     document.getElementById(selection).parentNode.classList.add("bg-success");
+    rightQuestions++;
   } else {
     console.log('Falsche Antwort!');
 
